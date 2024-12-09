55f32e500135844735a6c47e6752856cb517410d
KEANCA <kevin.castillo.ttc.571@unilibre.edu.co>
2024-12-09 18:50:38 -0500

    muchos cambios

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const baseDeDatos = [
-        { id: 1, nombre: 'Galleta de Nutella', precio: 1000, imagen: './assets/img/Galletas/Nutella.jpg', url: './vistas_productos/galleta1.html', categoria: 'Juguetes' },
-        { id: 2, nombre: 'Galleta de Mashmelo', precio: 2000, imagen: './assets/img/Galletas/Masmelo.jpg', url: './vistas_productos/galleta2.html', categoria: 'Mochilas' },
-        { id: 3, nombre: 'Galleta Red Velvet', precio: 1500, imagen: './assets/img/Galletas/RedVelvet.jpg', url: './vistas_productos/RedVelvet.html', categoria: 'Juguetes' },
-        { id: 4, nombre: 'Galleta de Bocadillo y Queso', precio: 1800, imagen: './assets/img/Galletas/Bocadillo y queso.jpg', url: './vistas_productos/bocadilloyqueso.html', categoria: 'Mochilas' },
-        { id: 5, nombre: 'Galleta de Macadamia', precio: 2200, imagen: './assets/img/Galletas/Macadamia.jpg', url: './vistas_productos/Macadamia.html', categoria: 'Juguetes' },
-        { id: 6, nombre: 'Galleta de Chocolate y Arequipe', precio: 2000, imagen: './assets/img/Galletas/Arequipe.jpg', url: './vistas_productos/Chocolateyarequipe.html', categoria: 'Mochilas' },
-        { id: 7, nombre: 'HolyBox', precio: 5000, imagen: './assets/img/Galletas/HolyBox.jpg', url: './vistas_productos/Holybox.html', categoria: 'Juguetes' }
+        { id: 1, nombre: 'Galleta de Nutella', precio: 1000, imagen: './assets/img/Galletas/Nutella.jpg', url: './vistas_productos/galleta1.html', categoria: 'Galletas' },
+        { id: 2, nombre: 'Galleta de Mashmelo', precio: 2000, imagen: './assets/img/Galletas/Masmelo.jpg', url: './vistas_productos/galleta2.html', categoria: 'Galletas' },
+        { id: 3, nombre: 'Galleta Red Velvet', precio: 1500, imagen: './assets/img/Galletas/RedVelvet.jpg', url: './vistas_productos/RedVelvet.html', categoria: 'Galletas' },
+        { id: 4, nombre: 'Galleta de Bocadillo y Queso', precio: 1800, imagen: './assets/img/Galletas/Bocadillo y queso.jpg', url: './vistas_productos/bocadilloyqueso.html', categoria: 'Galletas' },
+        { id: 5, nombre: 'Galleta de Macadamia', precio: 2200, imagen: './assets/img/Galletas/Macadamia.jpg', url: './vistas_productos/Macadamia.html', categoria: 'Galletas' },
+        { id: 6, nombre: 'Galleta de Chocolate y Arequipe', precio: 2000, imagen: './assets/img/Galletas/Arequipe.jpg', url: './vistas_productos/Chocolateyarequipe.html', categoria: 'Galletas' },
+        { id: 7, nombre: 'HolyBox', precio: 5000, imagen: './assets/img/Galletas/HolyBox.jpg', url: './vistas_productos/Holybox.html', categoria: 'Galletas' },
+        { id: 8, nombre: 'ChocoNutella', precio: 14000, imagen: './assets/img/chocotela.jpg', url: '#', categoria: 'Postres' },
+        { id: 9, nombre: 'ChipChocoTela', precio: 14000, imagen: './assets/img/chocochip.jpg', url: '#', categoria: 'Postres' },
+        { id: 10, nombre: 'RedTela', precio: 12000, imagen: './assets/img/chocoquipe.jpg', url: '#', categoria: 'Postres' },
+        { id: 11, nombre: 'ChocoLipe', precio: 12000, imagen: './assets/img/postre1.jpg', url: '#', categoria: 'Postres' },
+        { id: 12, nombre: 'Vaso de Helado', precio: 5000, imagen: './assets/img/helado.jpg', url: '#', categoria: 'Helados' }
     ];
 
     let carrito = JSON.parse(localStorage.getItem('carrito')) || []; 
