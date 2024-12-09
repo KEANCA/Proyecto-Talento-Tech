0a8e42a8acb60d94d537fe3339919b4b8535eec6
KEANCA <kevin.castillo.ttc.571@unilibre.edu.co>
2024-12-09 09:57:29 -0500

    se conecto el login con index

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const formContainer = document.querySelector(".form-container");
     const goToRegister = document.getElementById("goToRegister");
     const goToLogin = document.getElementById("goToLogin");
+    const loginForm = document.getElementById("loginForm");
 
     // Cambiar a vista de registro
     goToRegister.addEventListener("click", () => {
@@ -11,5 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
     // Cambiar a vista de login
     goToLogin.addEventListener("click", () => {
         formContainer.classList.remove("rotated");
+    });
+
+    // Manejar el envío del formulario de inicio de sesión
+    loginForm.addEventListener("submit", (event) => {
+        event.preventDefault(); 
+        window.location.href = "/index.html";
     });
 });
